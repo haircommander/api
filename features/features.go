8ab0c64bc88a7f54d759a273c759280434b24b5f
Peter Hunt <pehunt@redhat.com>
2026-08-21 09:45:52 -0400

    register DRADeviceTaintRules in all feature sets
    don't enable yet because of a merge ordering problem between API and CKASO
    
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

--- features/features.go
+++ features/features.go
@@ -1059,4 +1059,11 @@ var (
 							enhancementPR("https://github.com/openshift/enhancements/pull/2081").
 							enable(inDevPreviewNoUpgrade()).
 							mustRegister()
+
+	FeatureGateDRADeviceTaintRules = newFeatureGate("DRADeviceTaintRules").
+					reportProblemsToJiraComponent("node").
+					contactPerson("haircommander").
+					productScope(kubernetes).
+					enhancementPR("https://github.com/kubernetes/enhancements/issues/5055").
+					mustRegister()
 )
